f4d562d5717c619a1d23f16b240551d3fbba2c77
ismings <119486313+ismings@users.noreply.github.com>
2022-12-16 11:34:14 +0900

    [Silver III] Title: 병든 나이트, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Silver/1783. 병든 나이트/병든 나이트.py
@@ -0,0 +1,11 @@
+Y, X = map(int, input().split())
+
+if Y == 1:
+    print(1)
+elif Y == 2:
+    print(min(4, (X + 1) // 2))
+else:
+    if X <= 6:
+        print(min(4, X))
+    else:
+        print(X-2)
